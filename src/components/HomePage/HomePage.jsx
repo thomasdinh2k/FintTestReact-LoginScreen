8ce08db7e061dc6a8b1e04f87bba0573dcdc6b06
thomasdinh2k <work.dnstung@gmail.com>
2024-05-08 17:33:43 +0700

    create static version of the home page

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,6 +1,16 @@
 import React from "react"
 import NavBar from "./NavBar/navBar"
+import "./styles.scss"
 
 export default function HomePage() {
-	return <NavBar />
+	return (
+		<>
+			<div className="homepage-root">
+				<NavBar />
+				<h1>
+					Hello Site
+				</h1>
+			</div>
+		</>
+	)
 }
