246b4f89b4db1008bf4de8725b71ed3e5ecdbaf2
thomasdinh2k <work.dnstung@gmail.com>
2024-05-12 20:12:25 +0700

    add todo

--- src/components/pages/HomePage/NavBar/NavSection/NavList/NavItems/NavItemTitle.jsx
+++ src/components/pages/HomePage/NavBar/NavSection/NavList/NavItems/NavItemTitle.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 
 export default function NavItemTitle({ title, icon, hasItem, link }) {
+	// [ ] Allow Submenu open conditionally through State
 	return (
 		<Link to={link}>
 			<i className={icon}></i>
