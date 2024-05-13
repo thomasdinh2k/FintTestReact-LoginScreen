cc5428e8072c91f05a95df842f21e129bec547aa
thomasdinh2k <work.dnstung@gmail.com>
2024-05-13 14:33:11 +0700

    transfer routers from app.jsx to main.jsx as convention

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import "./styles/styles.scss"
-import { Route, Routes } from "react-router-dom"
-import HomePage from "./pages/Home"
-import LoginScreen from "./pages/Login"
-import SettingScreen from "./pages/Setting"
-import SignUpScreen from "./pages/SignUp"
 function App() {
 	return (
 		<>
-			<Routes>
+			{/* <Routes>
 				<Route path="/" element={<LoginScreen />} />
 				<Route path="sign-up" element={<SignUpScreen />} />
 				<Route path="/home" element={<HomePage />} />
 				<Route path="/settings" element={<SettingScreen />} />
-			</Routes>
+			</Routes> */}
 		</>
 	)
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,38 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 import { BrowserRouter } from "react-router-dom"
+import {
+	createBrowserRouter,
+	Route,
+	RouterProvider,
+	Routes,
+} from "react-router-dom"
+import HomePage from "./pages/Home"
+import LoginScreen from "./pages/Login"
+import SettingScreen from "./pages/Setting"
+import SignUpScreen from "./pages/SignUp"
+
+const routers = createBrowserRouter([
+	{
+		path: "/",
+		element: <LoginScreen />,
+	},
+	{
+		path: "/sign-up",
+		element: <SignUpScreen />,
+	},
+	{
+		path: "/home",
+		element: <HomePage />,
+	},
+	{
+		path: "/settings",
+		element: <SettingScreen />,
+	},
+])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<RouterProvider router={routers}></RouterProvider>
 	</React.StrictMode>
 )
