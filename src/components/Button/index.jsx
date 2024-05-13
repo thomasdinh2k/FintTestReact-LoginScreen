9b430f0997ab00582be6bb510b10e0a6a38acb68
thomasdinh2k <work.dnstung@gmail.com>
2024-05-13 15:29:43 +0700

    fix: properly import Button stylesheet

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,3 +1,5 @@
+import "./style.scss"
+
 export default function Button({type, children}) {
 	return (
 		<button className="gradient-button" type={type}>
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -2,24 +2,27 @@ import "./index.scss"
 import NavSection from "./NavSection"
 
 import navItem from "../../data/nav-item.json"
+import { useState } from "react"
 
 const NavBar = () => {
+	
+	const [isOpen, setIsOpen] = useState(true);
+	
+	const handleToggleNav = () => {
+		setIsOpen(!isOpen)
+	}
+	
 	return (
 		<>
-			<nav className="active">
+			<nav className={isOpen ? "active" : "closed"}>
 				{/* Nav Header */}
 				<div>
-					<div className="nav-toggle">
-						<i className="arrow left" />
+					<div className="nav-toggle" onClick={handleToggleNav}>
+						<i className={`arrow ${isOpen ? "left" : "right"}`} />
 					</div>
 					<div className="head">
 						<h1>Hello World</h1>
 						<p>FintSoftVN</p>
-						<p className="mini">
-							<strong>FINT</strong>
-							<br />
-							SoftVN
-						</p>
 					</div>
 				</div>
 				{Object.entries(navItem).map(([menuName, items]) => (
