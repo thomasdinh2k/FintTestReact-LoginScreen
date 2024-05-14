983dca26c52d9048b5a4862c427d6f0a8190384f
thomasdinh2k <work.dnstung@gmail.com>
2024-05-14 13:38:44 +0700

    styling: add glassmorphism effect on background and wrapper

--- src/components/Wrapper/index.jsx
+++ src/components/Wrapper/index.jsx
@@ -1,6 +1,13 @@
+import "./style.scss"
+
 export default function Wrapper({ children }) {
 	return (
 		<div className="theme-root">
+			<div className="bubbles">
+				<div className="bg-shape-1"></div>
+				<div className="bg-shape-2"></div>
+				<div className="bg-shape-3"></div>
+			</div>
 			<img
 				src="https://www.fint.vn/wp-content/uploads/2023/11/1626080287-12892745-fint-vn.png"
 				alt=""
